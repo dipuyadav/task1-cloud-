1ea9c951e3cbb8fea2f329827e455f01ba3f5be4
Deepanshu Yadav <56504637+dipuyadav@users.noreply.github.com>
2022-07-05 02:43:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-welcome to lw
-<br />
-<pre>
-<?php
-print 'ifconfig';
-?>
-</pre>
+<html>
+<body>
+<h1>OUR EARTH</h1>
+<img src="https://github.com/2sudhanhu/sudhanshu/blob/master/earth.gif?raw=true" width="700" height="600">
+</body>
+</html>
+
